6d5c093a3c80b3b3d6221994b33786bcead504fa
Xiaoming Xi <xiaomingxi@sifli.com>
2025-10-08 14:20:20 +0800

    Add pdf support

--- src/sifli_docs_toolbox/build_docs.py
+++ src/sifli_docs_toolbox/build_docs.py
@@ -39,31 +39,43 @@ def copy_static_files():
 
 
 def run_command(command, cwd=None):
+    print(f"Run command: {command}")
     result = subprocess.run(command, shell=True, cwd=cwd)
     if result.returncode != 0:
         print(f"Error executing {command}")
         exit(result.returncode)
 
 
-def make_html(version, lang):
-    print(f"Building HTML documentation for version {version}, language: {lang} ...")
+def make_doc(version, lang, is_pdf, non_clean):
+    if is_pdf:
+        print(f"Building PDF documentation for version {version}, language: {lang} ...")
+    else:
+        print(f"Building HTML documentation for version {version}, language: {lang} ...")
     if version:
         os.environ['SIFLI_DOC_VERSION'] = version
     os.environ['SIFLI_DOC_LANGUAGE'] = lang
-    run_command(f'sphinx-build -M html source/{lang} build/{lang} -j 8')
+    if not non_clean:
+        opts = '-a -E'
+    else:
+        opts = ''
 
+    if is_pdf:
+        run_command(f'sphinx-build -M simplepdf source/{lang} build/{lang} {opts} -j 8')
+    else:
+        run_command(f'sphinx-build -M html source/{lang} build/{lang} {opts} -j 8')
 
-def _main(version, lang):
-    # copy_static_files()
-    make_html(version, lang)
+def _main(version, lang, pdf, non_clean):
+    make_doc(version, lang, pdf, non_clean)
 
 def main():
     parser = argparse.ArgumentParser(description='Generate documentation.')
     parser.add_argument('--lang', choices=['en', 'zh_CN'], default='zh_CN', help='Specify language(en or zh_CN)')
     parser.add_argument('--version', type=str, help='Specify version')
+    parser.add_argument('--pdf', action="store_true", help='Build PDF')
+    parser.add_argument('--non-clean', action="store_true", help='non clean build')
     args = parser.parse_args()
 
-    _main(args.version, args.lang)
+    _main(args.version, args.lang, args.pdf, args.non_clean)
 
 if __name__ == "__main__":
     main()
--- src/sifli_docs_toolbox/docs_conf.py
+++ src/sifli_docs_toolbox/docs_conf.py
@@ -11,10 +11,12 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
 import os
+import sys
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 import datetime
+from pathlib import Path
 
 author = 'SiFli'
 
@@ -29,6 +31,8 @@ extensions = [
               "sphinx_selective_exclude.eager_only",
               "sphinx_selective_exclude.search_auto_exclude",
               "sphinx_last_updated_by_git",
+              "sphinx_simplepdf",
+              "sifli_docs_toolbox.extensions.gen_unique_ids"
 ]
 
 templates_path = ['../_templates']
@@ -40,7 +44,7 @@ html_last_updated_fmt = '%Y-%m-%d %H:%M:%S'
 current_year = datetime.datetime.now().year
 language = os.environ.get('SIFLI_DOC_LANGUAGE', 'zh_CN')
 if language == 'en':
-    copyright = '2019 - {} SiFli Technologies(Nanjing) Co., Ltd'.format(current_year)
+    copyright = '2019 - {} SiFli Technologies(Nanjing) Co., Ltd.'.format(current_year)
 else:
     copyright = '2019 - {} 思澈科技（南京）有限公司'.format(current_year)
 
@@ -61,6 +65,7 @@ html_context = {
         ("中文", f"./{version}/zh_CN/%s/", "zh_CN"),
     ],
     "current_version": version,
+    "curr_date": datetime.datetime.now().strftime("%Y-%m-%d")
 }
 
 docs_toolbox_dir = os.path.abspath(os.path.dirname(__file__))
@@ -93,6 +98,69 @@ myst_enable_extensions = [
     "tasklist",
 ]
 
+myst_heading_anchors = 6
 
+simplepdf_vars = {
+    'primary-opaque': 'rgba(31, 177, 170, 0.5)',
+    'primary': '#1fb1aa',
+    'secondary': '#961a1a',
+    'cover': '#000000',
+    'cover-bg': '#ffffff',
+    'white': '#ffffff',
+    'links': '#961a1a'
+}
+
+simplepdf_theme_options = {
+    'custom_header': True,
+    'custom_footer': True,
+    'cover_sifli': True,
+    'logo': '_static/logo_white_pdf.png',
+    'website': 'www.sifli.com'
+
+}
+
+def setup(app):
+    static_dir = Path(__file__).parent / "_static"
+
+    if 'simplepdf' in sys.argv:
+        is_simplepdf_builder = True
+    else:
+        is_simplepdf_builder = False
+
+    app.config.html_static_path.append(str(static_dir))
+
+    if is_simplepdf_builder:
+        app.add_css_file("pdf.css")
+
+    app.add_config_value('doc_id', None, 'env')
+    app.add_config_value('chip', None, 'env')
+    app.add_config_value('doc_name', None, 'env')
+    app.add_config_value('pdf_url_enabled', None, 'env')
+    app.add_config_value('doc_dir', None, 'env')
+
+    app.connect('config-inited',  update_config)
+
+    return {
+        "parallel_read_safe": True,
+        "parallel_write_safe": True,
+    }
+
+def update_config(app, config):
+    if 'simplepdf' in sys.argv:
+        is_simplepdf_builder = True
+    else:
+        is_simplepdf_builder = False
+
+    if app.config.doc_id and app.config.chip and app.config.doc_name:
+        pdf_filename = f"{app.config.doc_id}_{app.config.chip}_{app.config.doc_name}_{version}.pdf"
+    else:
+        pdf_filename = f"{app.config.proj}.pdf"
+
+    if is_simplepdf_builder and pdf_filename:
+        app.config.simplepdf_file_name = pdf_filename
+
+    if app.config.pdf_url_enabled:        
+        doc_path = f'{app.config.doc_dir}/{app.config.version}/{language}/{pdf_filename}'
+        html_theme_options['pdf_url'] = f"https://docs.sifli.com/projects/{doc_path}"
 
 
